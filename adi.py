2f65b701185915bad0ce06323080c2a8ca95f0ad
Adityakumar1607 <118658911+Adityakumar1607@users.noreply.github.com>
2024-11-17 03:47:06 +0530

    Update adi.py

--- adi.py
+++ adi.py
@@ -433,9 +433,9 @@ elif action == "No":
                     portfolioValue = sharesCanBuy * sellClosePrice + remainingCapital
                     netPosition = portfolioValue - capital
 
-                    return "You can buy {sharesCanBuy} shares of {name} at ({symbol}{buyClosePrice:.2f}) each. Your portfolio value at the end will be ({symbol}{portfolioValue:.2f}) with a net position of ({symbol}{netPosition:.2f}).".format(symbol, capital, symbol, buyClosePrice)
+                    return f"You can buy {sharesCanBuy} shares of {name} at ({/symbol}{buyClosePrice:.2f}) each. Your portfolio value at the end will be ({symbol}{portfolioValue:.2f}) with a net position of ({symbol}{netPosition:.2f})."
                 else:
-                    return "Your initial capital ({symbol}{capital:.2f}) is lower than the stock's opening price ({symbol}{buyClosePrice:.2f}). Unable to buy any shares.".format(symbol, capital, symbol, buyClosePrice)
+                    return f"Your initial capital ({symbol}{capital:.2f}) is lower than the stock's opening price ({symbol}{buyClosePrice:.2f}). Unable to buy any shares."
             else:
                 return f"Error: Starting date ({starting}) is greater than ending date ({ending})."
         else:
